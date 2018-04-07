fada986f74bf7cf28e68c73a9704a53d7036ac74
scloudyy <onecloud.shen@gmail.com>
2018-04-07 16:37:40 +0800

    init

---
+++ 18-insertion-sort/main.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+void insertion_sort(vector<int> &arr) {
+    int key;
+    for (auto j = arr.begin() + 1; j != arr.end(); j++) {
+        key = *j;
+        auto i = j - 1;
+        while(i >= arr.begin() && key < *i) {
+            *(i + 1) = *i;
+            i--;
+        }
+        *(i + 1) = key;
+    }
+}
+
+int main(int argc, char **argv) {
+    vector<int> arr = {3, 2, 5, 1, 7, 2, 4, 1, 0, 10, 5, -2};
+    insertion_sort(arr);
+
+    for (auto i : arr)
+        cout << i << " " << endl;
+}
